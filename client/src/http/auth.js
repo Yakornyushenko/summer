80fe917fc78501d3a5797872b3c4e547e1c0e75e
Gasello <9yar9@mail.ru>
2023-05-23 22:28:14 +0200

    commit message

--- client/src/http/auth.js
+++ client/src/http/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const auth = async () => {
     try {
-        const {data} = await axios.get('http://localhost:5000/api/auth/',
+        const {data} = await axios.get('http://localhost:10000/api/auth/',
             {params: {password: 'paralect123', login: 'sergei.stralenia@gmail.com'}});
         return data
     } catch (err) {
