3fd686ae4efdbb32714b44903415704115f7d5ec
Gasello <9yar9@mail.ru>
2023-05-23 20:53:57 +0200

    commit message

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import {AppModule} from "./app.module";
 import {ValidationPipe} from "@nestjs/common";
-import path from "path";
 
 async function start() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule, {cors: false});
-    // @ts-ignore
-    app.get("/*", function (req, res) {
-      res.sendFile(
-          path.join(__dirname, "../client/build/index.html"),
-          function (err) {
-            res.status(500).send(err)
-          }
-      )
-    })
+
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     app.enableCors({credentials: true, origin: true});
     await app.listen(PORT, () => console.log(`server started on port ${PORT}`));
