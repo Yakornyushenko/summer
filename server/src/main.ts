fa5174a7150464162fc78b7a479aeec173dcb6d4
Gasello <9yar9@mail.ru>
2023-05-24 11:49:09 +0200

    commit message

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function start() {
     const app = await NestFactory.create(AppModule, {cors: true});
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-    app.enableCors({credentials: true, origin: true});
+    // app.enableCors({credentials: true, origin: true});
     await app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (err) {
     console.log(err)
