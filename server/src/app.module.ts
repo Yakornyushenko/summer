3d3adc9ff01ce4cca89110b74516411c2691b5ee
Gasello <9yar9@mail.ru>
2023-05-24 15:58:57 +0200

    commit message

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,10 @@
 import {Module} from '@nestjs/common';
 import {VacanciesModule} from "./vacancies/Vacancies.module";
 import {AuthModule} from "./auth/Auth.Module";
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { join } from 'path';
 
 @Module({
 
     imports: [
-        // ServeStaticModule.forRoot({
-        //     rootPath: join(__dirname, '..', 'dist'),
-        //     renderPath: '*',
-        // }),
         AuthModule,
         VacanciesModule
     ],
