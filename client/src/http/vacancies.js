469510e4695ca335545bec42b00517b9d2205f8b
Gasello <9yar9@mail.ru>
2023-05-24 14:07:52 +0200

    commit message

--- client/src/http/vacancies.js
+++ client/src/http/vacancies.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export const getVacancy = async (id) => {
-    const {data} = await axios.get(`http://localhost:5000/api/${id}`)
+    const {data} = await axios.get(`https://summer-startup.onrender.com/api/${id}`)
     return data
 }
 export const getCategories = async () => {
-    const {data} = await axios.get('http://localhost:5000/api/catalogues/')
+    const {data} = await axios.get('https://summer-startup.onrender.com/catalogues/')
     return data
 }
 export const getVacancies = async ( accessToken, page, count, keyword, catalogues, payment_from, payment_to ) => {
-    const {data} = await axios.get('http://localhost:5000/api/vacancies', {
+    const {data} = await axios.get('https://summer-startup.onrender.com/api/vacancies', {
         params: {
             page,
             count,
