3a23bf6915d7c889261a5d94b8959d26588d21a8
Gasello <9yar9@mail.ru>
2023-05-24 12:35:12 +0200

    commit message

--- server/dist/main.js
+++ server/dist/main.js
@@ -6,9 +6,8 @@ const common_1 = require("@nestjs/common");
 async function start() {
     try {
         const PORT = process.env.PORT || 5000;
-        const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: false });
+        const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
         app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true }));
-        app.enableCors({ credentials: true, origin: true });
         await app.listen(PORT, () => console.log(`server started on port ${PORT}`));
     }
     catch (err) {
